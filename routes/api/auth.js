040fd8b6ce5e7e07b5b76dae448f2c01126e394d
Pumpon4yk <tt10392@gmail.com>
2023-05-22 20:35:39 +0200

    beta ver1.1

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,13 +6,13 @@ const {schemasUsers} = require ('../../schemas');
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemasUsers.register), controller.register);
+router.post("/users/register", validateBody(schemasUsers.register), controller.register);
 
-router.post("/login", validateBody(schemasUsers.login), controller.login);
+router.post("/users/login", validateBody(schemasUsers.login), controller.login);
 
-router.post("/logout", authValidToken, controller.logout);
+router.post("/users/logout", authValidToken, controller.logout);
 
-router.get("/current", authValidToken, controller.current);
+router.get("/users/current", authValidToken, controller.current);
 
 router.patch("/users", authValidToken, validateBody(schemasUsers.subscription), controller.subscription);
 
